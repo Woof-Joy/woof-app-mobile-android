fa7c07f704b1ef55a37aee0602fd8c5ed33bdcb5
pedrooliveiralopes <pedro.lopes@sptech.school>
2024-05-20 19:22:56 -0300

    Adicionando o /api

--- app/src/main/java/com/woofjooy/client/RetrofitClient.kt
+++ app/src/main/java/com/woofjooy/client/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://localhost:8080/api"
+    private const val BASE_URL = "http://10.18.32.81:8080/api"
     val instance: ServicesBack by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
