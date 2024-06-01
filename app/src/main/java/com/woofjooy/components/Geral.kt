796a276f38d0d173eaf1dfa0e4f5154e173bcd20
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2024-06-01 12:37:45 -0300

    ajustando tela

--- app/src/main/java/com/woofjooy/components/Geral.kt
+++ app/src/main/java/com/woofjooy/components/Geral.kt
@@ -128,7 +128,7 @@ fun InputSelect(searchText: MutableState<String>, options: List<String>, label:S
                 .width(80.dp)
                 .padding(top = 5.dp, end = 5.dp)
                 .background(
-                    color = colorResource(R.color.cinza_legenda),
+                    color = colorResource(R.color.cinza),
                     shape = RoundedCornerShape(15.dp)
                 )
             ,
